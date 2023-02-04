9df3901112894fab4c57a0c1c9f8ed2537d7305c
Lipraty <sis1943@outlook.com>
2023-02-04 18:17:05 +0800

    chore: change usage infomation

--- src/index.ts
+++ src/index.ts
@@ -18,13 +18,19 @@ namespace Yunzai {
 ### 插件说明
 
 该插件是 Yunzai 插件兼容加载器，请前往插件市场搜索 \`kunzai\` 来获取 yunzai 插件。 
+
+### 关于数据存储
+
+ \`redis\` 模式：可以连接原数据库来兼容旧数据，但不保证在 Koishi 下数据的准确性
+
+ \`cache\` 模式：标准的数据存储，拥有更好的性能以及准确性
 `
   export interface Config {
     storeCompatible: string
    }
 
   export const Config: Schema<Config> = Schema.object({
-    storeCompatible: Schema.union(['kvdata', 'redis']).default('kvdata').description('数据存储方式')
+    storeCompatible: Schema.union(['cache', 'redis']).default('cache').description('数据存储方式')
   })
 }
 
