85624066b916ceab48b39004b466b891e62efea9
Lipraty <sis1943@outlook.com>
2023-02-28 16:50:58 +0800

    chore: add genshin serivce

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,20 @@
 import { Context, Schema, Service } from 'koishi'
+import { } from '@koishijs/cache'
+import { GenshinAPI } from './api'
 
 declare module 'koishi' {
   interface Context {
     yunzai: Yunzai
+    genshin: GenshinAPI
   }
 }
 
 class Yunzai extends Service {
-  usage = Yunzai.usage
+  readonly usage = Yunzai.usage
+
   constructor(ctx: Context, private config: Yunzai.Config) {
     super(ctx, 'yunzai', true)
+
   }
 }
 
@@ -17,21 +22,11 @@ namespace Yunzai {
   export const usage = `
 ### 插件说明
 
-该插件是 Yunzai 插件兼容加载器，请前往插件市场搜索 \`kunzai\` 来获取 yunzai 插件。 
-
-### 关于数据存储
-
- \`redis\` 模式：可以连接原数据库来兼容旧数据，但不保证在 Koishi 下数据的准确性
-
- \`cache\` 模式：标准的数据存储，拥有更好的性能以及准确性
+该插件是 Yunzai 插件兼容层，请前往插件市场搜索 \`kunzai\` 来获取 yunzai 插件。 
 `
-  export interface Config {
-    storeCompatible: string
-   }
+  export interface Config { }
 
-  export const Config: Schema<Config> = Schema.object({
-    storeCompatible: Schema.union(['cache', 'redis']).default('cache').description('数据存储方式')
-  })
+  export const Config: Schema<Config> = Schema.object({})
 }
 
 export default Yunzai
