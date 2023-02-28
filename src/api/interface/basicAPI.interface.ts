b7e4bf37a67e15ce05704d6a06c946cb88d799b5
Lipraty <sis1943@outlook.com>
2023-02-28 16:47:09 +0800

    chore: stand alone `api`

--- src/api/interface/basicAPI.interface.ts
+++ src/api/interface/basicAPI.interface.ts
@@ -1,5 +1,9 @@
+type APIRequestMethod = 'GET' | 'POST'
+type APIType = keyof APIRegionOption
+type APIParameTyper = 'string' | 'number' | 'boolean' | 'json'
+
 /**
- * 基本API请求列表。
+ * 基本 API 请求列表。
  */
 type APIStencil = {
     readonly [K in string]: APIStencilOption
@@ -24,10 +28,6 @@ interface APIRegionOption {
     readonly record: string
 }
 
-type APIRequestMethod = 'GET' | 'POST'
-type APIType = keyof APIRegionOption
-type APIParameTyper = 'string' | 'number' | 'boolean' | 'json'
-
 interface APIList {
     bbsSign: APIOption
     bbsSignHome: APIOption
@@ -36,17 +36,21 @@ interface APIList {
 }
 
 /**
- * API请求对象选项
+ * 兼容的 API 请求对象选项
  */
 interface APIOption {
-    /**API Host类型，如果为undefined，应该提供完整URL。*/
-    type: 'takumi' | 'hk4e' | 'record' | undefined
-    /**请求API URL不存在PUT、DELETE等具有数据库写入动作的情况 */
-    method: 'POST' | 'post' | 'GET' | 'get'
-    /**如果type为undefined，应该提供完整URL。*/
-    url: string
+    /**该 API 适用地区 */
+    availableFor: ('china' | 'overseas')[]
+    /**API Host 类型，如果为 undefined，应该提供完整 URL。*/
+    hostBy?: APIType | Record<'china' | 'overseas', APIType | undefined>
+    /**API 需要的参数 */
+    parameters: ([string, APIParameTyper, boolean?])[]
+    /**请求米游社 API URL 不存在 PUT、DELETE 等具有数据库写入动作的情况 */
+    method: APIRequestMethod
+    /**如果 type 为 undefined，应该提供完整 URL。*/
+    url: string | Record<'china' | 'overseas', string>
     /**限制请求参数（GET:Query|POST:Body）*/
     params?: readonly string[]
-    /**这个API是否需要cookie */
+    /**这个 API 是否需要 cookie */
     cookie?: boolean
 }
